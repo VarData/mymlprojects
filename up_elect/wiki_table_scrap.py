b287660f2aad4e9b26ee2be8201533e3d515e142
VarData <ds.sanjeetshukla@outlook.com>
2017-02-06 09:05:03 +0530

    Update wiki_table_scrap.py

--- up_elect/wiki_table_scrap.py
+++ up_elect/wiki_table_scrap.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 from pandas import DataFrame
-import urllib.request
+import urllib.request as urr
 import os
 import codecs
+import csv
 
 # def make_soup(url):
 #     thepage=urllib.request.urlopen(url)
@@ -15,17 +16,19 @@ header = {'User-Agent': 'Mozilla/5.0'} #Needed to prevent 403 error on Wikipedia
 req = urr.Request(wiki,headers=header)
 page = urr.urlopen(req)
 soup = BeautifulSoup(page, "lxml")
+out = csv.writer(open("myfile.csv","w"), delimiter=',',quoting=csv.QUOTE_ALL)
 
-soup= make_soup("https://en.wikipedia.org/wiki/Sixteenth_Legislative_Assembly_of_Uttar_Pradesh")
+#soup= make_soup("https://en.wikipedia.org/wiki/Sixteenth_Legislative_Assembly_of_Uttar_Pradesh")
 
-albumdatasaved="#,Assembly,Name,Party,Reserved,ID,District,LS,Comments"
+lacDataSaved="#,Assembly,Name,Party,Reserved,ID,District,LS,Comments"
 # find all table ,get the first
 table = soup.find_all('table', class_="wikitable")[4] # Only use the first table
 # iter over it
 for record in table.findAll('tr'):
-    albumdata=""
+    lacData=""
     for data in record.findAll('td'):
-        albumdata=albumdata+","+data.text
-    albumdatasaved=albumdatasaved+"\n"+albumdata[1:]
+        lacData=lacData+","+data.text
+    lacDataSaved=lacDataSaved+"\n"+lacData[1:]
+    out.writerow(lacDataSaved)
 
-print(albumdatasaved)
+#print(lacDataSaved)
